c9174aab99c3e896b7050406cb256189fdb1a965
palmerigarrett <palmerigarrett@gmail.com>
2023-06-27 19:48:58 -0400

    update picture

--- app/_components/Sidebar/Sidebar.tsx
+++ app/_components/Sidebar/Sidebar.tsx
@@ -37,7 +37,7 @@ const Sidebar = () => {
   return (
     <nav className={`${styles.sidebar} ${state.viewMode === "dark" ? '' : styles.light}`}>
       <Image
-        src="/portfolio_image.jpg"
+        src="/portfolio.jpg"
         alt="Garrett Palmeri Headshot"
         height={200}
         width={200}
